6fb226acf57556e81cab3cf5f02385c49a4422df
Akasha Peppermint <>
2022-04-17 17:07:25 +1000

    Fixed a bug where navigational commands were causing scroll restoration to emit.
    those commands were things like end-of-buffer, beginning-of-buffer, forward/backward-sexp, etc.
    which were likely to cause the point to move beyond the visible portion of the buffer.
    
    this was done by fixing a bug and adding an additional check to last-command. some more feature additions may be present

--- infiniscroll.el
+++ infiniscroll.el
@@ -13,60 +13,95 @@ infiniscroll mode will try to restore the original position of
 `point' after executing a sequence of any of these commands."
   :type 'hook)
 
+(defun infiniscroll-not-comint-buffer ()
+  "comint has their own scroll restore system usually.
+this would just interfere or something."
+  (derived-mode-p 'comint-mode))
+
+(defun infiniscroll-not-sexp-command ()
+  (not (member last-command '(forward-sexp backward-sexp forward-char backward-char))))
+
+(defcustom infiniscroll-buffer-ignore-fns 
+  '(minibufferp infiniscroll-not-comint-buffer)
+  "Predicates that run to tell infiniscroll which (current-buffer)s not to run in"
+  :type 'hook)
+
 (defvar-local infiniscroll-restore-ctype cursor-type)
+(defvar-local infiniscroll-touched nil)
+
+(defun infiniscroll-purge ()
+  (interactive)
+  (cl-loop for b in (buffer-list)
+        do (with-current-buffer b
+             (if infiniscroll-touched
+                 (setq cursor-type infiniscroll-restore-ctype))))
+  (cl-loop for w in (window-list)
+        if (window-parameter w 'infiniscroll-buffer)
+        do (set-window-parameter w 'infiniscroll-buffer (list))))
 
 (defun infiniscroll-post-command-logic () 
-  ;; Make cursor visible or invisible
-  (let* ((bfs (window-parameter (selected-window) 'infiniscroll-buffer))
-         (pt (cl-loop for m in bfs
-                   if (eq (marker-buffer m) (current-buffer))
-                   do (cl-return m))))
-    (if (markerp pt)
-        (if (not (pos-visible-in-window-p pt (selected-window))) 
-            (setf cursor-type nil)
-          (setf cursor-type infiniscroll-restore-ctype)))))
+  (if (notany #'funcall infiniscroll-buffer-ignore-fns) 
+      ;; Make cursor visible or invisible
+      (let* ((bfs (window-parameter (selected-window) 'infiniscroll-buffer))
+             (pt (cl-loop for m in bfs
+                       if (eq (marker-buffer m) (current-buffer))
+                       do (cl-return m))))
+        (when (markerp pt)
+          ;; (message "%s %s %s %s %s" last-command this-command (marker-position pt) (point) (not (member this-command infiniscroll-commands)))
+          (if (and (not (member this-command infiniscroll-commands))
+                   ;; This has to be here because somehow its effect affects infiniscroll-pre-command-logic restoration somehow
+                   ;; without it, stuff like end-of-buffer will cause a scroll restore before a self-insert-command
+                   (not (member last-command infiniscroll-commands)))
+              ;; (setf pt (point))
+              (setf (marker-position pt) (point))
+            (if (pos-visible-in-window-p pt (selected-window)) 
+                (setf cursor-type infiniscroll-restore-ctype)
+              (setf cursor-type nil
+                    infiniscroll-touched t)))))))
 
 (defun infiniscroll-pre-command-logic ()
-  (let* ((bfs (window-parameter (selected-window) 'infiniscroll-buffer))
-         (pt (cl-loop for m in bfs
-                   if (eq (marker-buffer m) (current-buffer))
-                   do (cl-return m))))
-    (if (markerp pt)
-        (if (eq this-command 'self-insert-command) 
-            (progn
-              (setf cursor-type infiniscroll-restore-ctype)
-              (if (pos-visible-in-window-p pt (selected-window))
-                  (setf (marker-position pt) (point))
-                
-                ;; Restore the position of the scroll point 
-                (map nil #'funcall scroll-pre-jump-back-hook)
-                (setf (point) pt)
-                (map nil #'funcall scroll-post-jump-back-hook)
-                ;; we sneakily forget old buffers here and stuff after jumping back. Could be done whenever.
-                (set-window-parameter (selected-window) 'infiniscroll-buffer
-                                      (cl-loop for m in bfs
-                                            if (buffer-live-p (marker-buffer m))
-                                            collect m 
-                                            else do (setf (marker-buffer m) nil)))))
+  (if (notany #'funcall infiniscroll-buffer-ignore-fns) 
+      (let* ((bfs (window-parameter (selected-window) 'infiniscroll-buffer))
+             (pt (cl-loop for m in bfs
+                       if (eq (marker-buffer m) (current-buffer))
+                       do (cl-return m))))
+        (if (markerp pt)
+            (if (eq this-command 'self-insert-command) 
+                (progn
+                  (setf cursor-type infiniscroll-restore-ctype)
+                  (if (pos-visible-in-window-p pt (selected-window))
+                      (setf (marker-position pt) (point))
+                    
+                    ;; Restore the position of the scroll point 
+                    (map nil #'funcall scroll-pre-jump-back-hook)
+                    (setf (point) pt)
+                    (map nil #'funcall scroll-post-jump-back-hook)
+                    ;; we sneakily forget old buffers here and stuff after jumping back. Could be done whenever.
+                    (set-window-parameter (selected-window) 'infiniscroll-buffer
+                                          (cl-loop for m in bfs
+                                                if (buffer-live-p (marker-buffer m))
+                                                collect m 
+                                                else do (setf (marker-buffer m) nil)))))
 
-          ;; Make cursor visible or invisible
-          (unless (member this-command infiniscroll-commands)
-            (setf (marker-position pt) (point)))
-          (if (not (pos-visible-in-window-p pt (selected-window))) 
-              (setf cursor-type nil)
-            (setf cursor-type infiniscroll-restore-ctype
-                  (point) pt))
-          ;; (message "%s movetime" this-command)
-          )
-      (let ((marker (point-marker)))
-        (set-marker-insertion-type marker t)
-        (set-window-parameter (selected-window) 'infiniscroll-buffer (cons marker bfs))))))
+              ;; Make cursor visible or invisible
+              (unless (member this-command infiniscroll-commands)
+                (setf (marker-position pt) (point)))
+              (if (not (pos-visible-in-window-p pt (selected-window))) 
+                  (setf cursor-type nil)
+                (setf cursor-type infiniscroll-restore-ctype
+                      (point) pt))
+              ;; (message "%s movetime" this-command)
+              )
+          (let ((marker (point-marker)))
+            (set-marker-insertion-type marker t)
+            (set-window-parameter (selected-window) 'infiniscroll-buffer (cons marker bfs)))))))
 
 (define-minor-mode infiniscroll-mode
     ""
   :global t
   :init-value nil
   :keymap nil
+  (infiniscroll-purge)
   (if infiniscroll-mode 
       (progn
         (add-hook 'pre-command-hook #'infiniscroll-pre-command-logic)
